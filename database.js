759cb3918be4fbfa90a879795b27e8c40beb181b
Kwok Ming Leung <k4leung@ucsd.edu>
2020-05-12 16:28:44 -0700

    added different CRUD command

--- database.js
+++ database.js
@@ -34,13 +34,44 @@ app.get('/createdb', (req, res) => {
 
 // Create table
 app.get('/createtable', (req, res) =>{
-    let sql = 'CREATE TABLE table1(id int AUTO_INCREMENT, text VARCHAR(255), primary key (id))';
+    let sql = 'CREATE TABLE table2(id int AUTO_INCREMENT, text VARCHAR(255), primary key (id))';
     db.query(sql, (err, result) => {
         if(err) throw err;
         console.log(result);
         res.send('Table created...');
     });
 });
+
+// Read/Print table
+app.get('/readtable', (req, res) =>{
+    let sql = 'SELECT * from table2';
+    db.query(sql, (err, rows, fields) => {
+        if(err) throw err;
+        console.log('Table printed');
+        res.send(rows);
+    });
+});
+
+// Insert values into table
+app.get('/insertintotable', (req, res) =>{
+    let sql = "INSERT INTO table2 (text) VALUES ('this is first')";
+    db.query(sql, (err, rows, fields) => {
+        if(err) throw err;
+        console.log('info inserted...');
+        res.send('info inserted...');
+    });
+});
+
+// Update table's value
+app.get('/updatetable', (req, res) =>{
+    let sql = "UPDATE table2 SET text = 'this is 3rd' WHERE id = 3";
+    db.query(sql, (err, rows, fields) => {
+        if(err) throw err;
+        console.log('table updated...');
+        res.send('table updated...');
+    });
+});
+
 
 app.listen('3000', () =>{
     console.log('Server started on port 3000');
