4dc9ebd6303c24b207f4a85e588ae3b19ab8bb0e
Azirly <jylin85@gmail.com>
2020-05-23 16:37:22 -0700

    Rough Draft Name Filter

--- root/src/js/raptor_name.js
+++ root/src/js/raptor_name.js
@@ -1,7 +1,18 @@
 var submitButton = document.getElementById('raptorNameSubmit');
+// import Filter from 'bad-words';
 
 submitButton.addEventListener('click', event => {
     var raptorNameText = document.getElementsByName('raptorName')[0].value;
+    // filter = new Filter();
+    // if (filter.isProfane("Fuck")) {
+    //     alert("Name must be appropriate");
+    //     return false;
+    // }
+    const bad_words = ['FUCK', 'FK', 'SHIT', 'ASS'];
+    if (bad_words.includes(raptorNameText.trim().toUpperCase())) {
+        alert('Name must be appropriate');
+        return false;
+    }
     console.log(document.getElementsByName('raptorName'));
     console.log(raptorNameText);
     var raptorName = {
