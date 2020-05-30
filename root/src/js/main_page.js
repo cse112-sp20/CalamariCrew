154ad6b596901ec60377e74041b9fb7f9033c71e
jsonavalos <jsonavalos>
2020-05-30 09:02:43 -0700

    Unit Test

--- root/src/js/main_page.js
+++ root/src/js/main_page.js
@@ -3,20 +3,15 @@ if (localStorage.getItem('raptor_name')) {
         'userVelocityRaptorName'
     ).innerHTML = localStorage.getItem('raptor_name');
 }
-
 if (localStorage.length != 0) {
     //get everything and activate listeners to dress raptor with info from other page
     ///switch cases with same ids for both
     const list = ['tail', 'back', 'head', 'hand'];
-
     for (bodyPart of list) {
         if (localStorage.getItem(bodyPart) != null) {
             document.getElementById(
                 localStorage.getItem(bodyPart)
             ).style.display = 'block';
-        } else {
-            //should remove the ones that are no longer selected
-            // document.getElementById(localStorage.getItem(bodyPart)).style.display="none";
         }
     }
 }
