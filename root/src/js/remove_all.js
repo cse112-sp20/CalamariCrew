aa4161e80e33cc0a3f20ba44282fc7cb5f9d9498
Ctakayama <cole.takayama@gmail.com>
2020-06-02 03:46:05 -0700

    new buttons, full colors, font changes, etc

--- root/src/js/remove_all.js
+++ root/src/js/remove_all.js
@@ -1,9 +1,23 @@
 const rm = document.getElementById('remove_all');
+var delayInMilliseconds = 1700; //1 second
+
+function restartGif(imgElement) {
+    let element = document.getElementById(imgElement);
+    element.style.zIndex = 3;
+    if (element) {
+        var imgSrc = element.src;
+        element.src = imgSrc;
+    }
+}
+
 rm.addEventListener('click', () => {
-    window.localStorage.removeItem('github_username');
-    window.localStorage.removeItem('token');
-    window.localStorage.removeItem('raptor_name');
-    // fetch('http://localhost:3000/clear');
-    document.getElementById('clearAll').click();
-    window.location.href = '/root/html/setup/auth.html';
+    restartGif('meteor');
+    setTimeout(() => {
+        window.localStorage.removeItem('github_username');
+        window.localStorage.removeItem('token');
+        window.localStorage.removeItem('raptor_name');
+        // fetch('http://localhost:3000/clear');
+        document.getElementById('clearAll').click();
+        window.location.href = '/root/html/setup/auth.html';
+    }, delayInMilliseconds);
 });
