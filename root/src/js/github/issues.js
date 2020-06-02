0775e06028e6650d3c3fe294fd2fd60ad2fdf820
akassab <andrewkassab@live.com>
2020-06-02 16:50:53 -0700

    actually merged

--- root/src/js/github/issues.js
+++ root/src/js/github/issues.js
@@ -51,80 +51,6 @@ export async function fetchUsername(token) {
                 throw 'Username could not be retrieved';
             });
 
-<<<<<<< HEAD
-    if (localStorage.getItem('repository')) {
-        var repo = JSON.parse(localStorage.getItem('repository'));
-        console.log(repo.repoId);
-        var repoName = document.getElementById('div-5');
-        repoName.innerHTML = `Repository: ${repo.repoId}`;
-        if (repo.repoId && repo.issueUrl && token) {
-            fetch(repo.issueUrl, {
-                headers: {
-                    // Include the token in the Authorization header
-                    Authorization: 'token ' + token,
-                },
-            })
-                .then(res => res.json())
-                .then(res => {
-                    res = res.filter(e => {
-                        if (!e.assignee) {
-                            return false;
-                        }
-
-                        var found = false;
-                        e.assignees.forEach(assignee => {
-                            if (assignee.login == username) {
-                                found = true;
-                            }
-                        });
-
-                        return found;
-                    });
-                    var issueList = document.getElementById('githubIssuesList');
-                    var speed = document.getElementById('div-4');
-
-                    res.forEach(issue => {
-                        let listElement = document.createElement('li');
-                        listElement.id = issue.number.toString();
-
-                        let inputElement = document.createElement('input');
-                        inputElement.type = 'checkbox';
-                        inputElement.id = 'checkbox' + issue.number.toString();
-
-                        inputElement.addEventListener('change', function() {
-                            if (this.checked) {
-                                // var response = confirm(
-                                //     'Do you want to close this issue?'
-                                // );
-                                // if (response == true) {
-                                closeIssue(
-                                    repo.issueUrl +
-                                        '/' +
-                                        issue.number.toString(),
-                                    token
-                                );
-                                // } else {
-                                //     inputElement.checked = false;
-                                // }
-                                issueList.removeChild(listElement);
-                            }
-                        });
-
-                        let link = document.createElement('a');
-                        link.title = issue.title;
-                        link.href = issue.html_url;
-                        link.target = '_blank';
-
-                        let linkText = document.createTextNode(issue.title);
-
-                        link.appendChild(linkText);
-                        listElement.appendChild(inputElement);
-                        listElement.appendChild(link);
-                        issueList.appendChild(listElement);
-                    });
-                });
-        }
-=======
         return username;
     }
 }
@@ -144,7 +70,6 @@ export async function retrieveIssues(token, username, repo) {
             .catch(err => {
                 throw 'Issues could not be retrieved';
             });
->>>>>>> 65ef21f5693d843b59ec89175da6741782d2123a
     }
 }
 
