21731ad9489156bee3a048d0167128604cfd3810
Liron <lborwick@ucsd.edu>
2020-06-06 18:05:19 -0700

    refactored main_page.js to be more testable

--- root/src/js/github/issues.js
+++ root/src/js/github/issues.js
@@ -1,108 +1,108 @@
-const token = localStorage.getItem('token');
+// const token = localStorage.getItem('token');
 
-getGithubIssues(token);
+// getGithubIssues(token);
 
-function getGithubIssues(token) {
-    var username;
-    if (localStorage.getItem('github_username')) {
-        username = localStorage.getItem('github_username');
-    } else {
-        fetch('https://api.github.com/user', {
-            headers: {
-                // Include the token in the Authorization header
-                Authorization: 'token ' + token,
-            },
-        })
-            .then(res => res.json())
-            .then(res => (username = res.login));
-    }
+// function getGithubIssues(token) {
+//     var username;
+//     if (localStorage.getItem('github_username')) {
+//         username = localStorage.getItem('github_username');
+//     } else {
+//         fetch('https://api.github.com/user', {
+//             headers: {
+//                 // Include the token in the Authorization header
+//                 Authorization: 'token ' + token,
+//             },
+//         })
+//             .then(res => res.json())
+//             .then(res => (username = res.login));
+//     }
 
-    if (localStorage.getItem('repository')) {
-        var repo = JSON.parse(localStorage.getItem('repository'));
+//     if (localStorage.getItem('repository')) {
+//         var repo = JSON.parse(localStorage.getItem('repository'));
 
-        if (repo.repoId && repo.issueUrl && token) {
-            fetch(repo.issueUrl, {
-                headers: {
-                    // Include the token in the Authorization header
-                    Authorization: 'token ' + token,
-                },
-            })
-                .then(res => res.json())
-                .then(res => {
-                    res = res.filter(e => {
-                        if (!e.assignee) {
-                            return false;
-                        }
+//         if (repo.repoId && repo.issueUrl && token) {
+//             fetch(repo.issueUrl, {
+//                 headers: {
+//                     // Include the token in the Authorization header
+//                     Authorization: 'token ' + token,
+//                 },
+//             })
+//                 .then(res => res.json())
+//                 .then(res => {
+//                     res = res.filter(e => {
+//                         if (!e.assignee) {
+//                             return false;
+//                         }
 
-                        var found = false;
-                        e.assignees.forEach(assignee => {
-                            if (assignee.login == username) {
-                                found = true;
-                            }
-                        });
+//                         var found = false;
+//                         e.assignees.forEach(assignee => {
+//                             if (assignee.login == username) {
+//                                 found = true;
+//                             }
+//                         });
 
-                        return found;
-                    });
-                    var issueList = document.getElementById('githubIssuesList');
-                    var speed = document.getElementById('div-4');
+//                         return found;
+//                     });
+//                     var issueList = document.getElementById('githubIssuesList');
+//                     var speed = document.getElementById('div-4');
 
-                    res.forEach(issue => {
-                        let listElement = document.createElement('li');
-                        listElement.id = issue.number.toString();
+//                     res.forEach(issue => {
+//                         let listElement = document.createElement('li');
+//                         listElement.id = issue.number.toString();
 
-                        let inputElement = document.createElement('input');
-                        inputElement.type = 'checkbox';
-                        inputElement.id = 'checkbox' + issue.number.toString();
+//                         let inputElement = document.createElement('input');
+//                         inputElement.type = 'checkbox';
+//                         inputElement.id = 'checkbox' + issue.number.toString();
 
-                        inputElement.addEventListener('change', function() {
-                            if (this.checked) {
-                                var response = confirm(
-                                    'Do you want to close this issue?'
-                                );
-                                if (response == true) {
-                                    closeIssue(
-                                        repo.issueUrl +
-                                            '/' +
-                                            issue.number.toString(),
-                                        token
-                                    );
-                                } else {
-                                    inputElement.checked = false;
-                                }
-                            }
-                        });
+//                         inputElement.addEventListener('change', function() {
+//                             if (this.checked) {
+//                                 var response = confirm(
+//                                     'Do you want to close this issue?'
+//                                 );
+//                                 if (response == true) {
+//                                     closeIssue(
+//                                         repo.issueUrl +
+//                                             '/' +
+//                                             issue.number.toString(),
+//                                         token
+//                                     );
+//                                 } else {
+//                                     inputElement.checked = false;
+//                                 }
+//                             }
+//                         });
 
-                        let link = document.createElement('a');
-                        link.title = issue.title;
-                        link.href = issue.html_url;
-                        link.target = '_blank';
+//                         let link = document.createElement('a');
+//                         link.title = issue.title;
+//                         link.href = issue.html_url;
+//                         link.target = '_blank';
 
-                        let linkText = document.createTextNode(issue.title);
+//                         let linkText = document.createTextNode(issue.title);
 
-                        link.appendChild(linkText);
-                        listElement.appendChild(inputElement);
-                        listElement.appendChild(link);
-                        issueList.appendChild(listElement);
-                    });
-                });
-        }
-    }
-}
+//                         link.appendChild(linkText);
+//                         listElement.appendChild(inputElement);
+//                         listElement.appendChild(link);
+//                         issueList.appendChild(listElement);
+//                     });
+//                 });
+//         }
+//     }
+// }
 
-function closeIssue(issueUrl, token) {
-    fetch(issueUrl, {
-        method: 'PATCH',
-        body: JSON.stringify({
-            state: 'closed',
-        }),
-        headers: {
-            // Include the token in the Authorization header
-            Authorization: 'token ' + token,
-        },
-    })
-        // Parse the response as JSON
-        .then(res => res.json())
-        .then(res => {
-            console.log(res);
-        });
-}
+// function closeIssue(issueUrl, token) {
+//     fetch(issueUrl, {
+//         method: 'PATCH',
+//         body: JSON.stringify({
+//             state: 'closed',
+//         }),
+//         headers: {
+//             // Include the token in the Authorization header
+//             Authorization: 'token ' + token,
+//         },
+//     })
+//         // Parse the response as JSON
+//         .then(res => res.json())
+//         .then(res => {
+//             console.log(res);
+//         });
+// }
--- root/src/js/main_page.js
+++ root/src/js/main_page.js
@@ -1,11 +1,11 @@
+const token = localStorage.getItem('token');
+const speed = document.getElementById('div-4');
 const NO_MILESTONE_MSG =
     'Please add a new milestone to start tracking velocity.';
 const NO_ISSUES_MSG = 'Please add a new issue to start tracking velocity.';
 const NO_MILESTONE = 1;
 const NO_ISSUES = 2;
-
-const token = localStorage.getItem('token');
-const speed = document.getElementById('div-4');
+const USER_URL = 'https://api.github.com/user';
 
 if (localStorage.getItem('raptor_name')) {
     document.getElementById(
@@ -13,38 +13,31 @@ if (localStorage.getItem('raptor_name')) {
     ).innerHTML = localStorage.getItem('raptor_name');
 }
 
-function getMostRecentMilestone() {
+async function getMostRecentMilestone() {
     const userName = localStorage.getItem('github_username');
-    const repo = JSON.parse(localStorage.getItem('repository'));
-    const repoName = repo.repoId;
-    fetch(`https://api.github.com/repos/${userName}/${repoName}/milestones`, {
-        headers: {
-            //get all issues
-            Authorization: 'token ' + token,
-        },
-    })
-        .then(res => res.json())
-        .then(milestones => {
-            if (milestones.length === 0) {
-                return null;
-            }
-            return milestones[0];
-        });
+    const repoName = JSON.parse(localStorage.getItem('repository')).repoId;
+    const repoUrl = `https://api.github.com/repos/${userName}/${repoName}/milestones`;
+
+    const milestones = await fetchUrl(repoUrl);
+    return milestones && milestones.length > 0 ? milestones[0] : null;
 }
 
-function setUser() {
-    fetch('https://api.github.com/user', {
-        headers: {
-            Authorization: 'token ' + token,
-        },
-    })
-        .then(res => res.json())
-        .then(name => {
-            localStorage.setItem('github_username', name.login);
-        });
+async function setUser() {
+    const res = await fetchUrl(USER_URL);
+    localStorage.setItem('github_username', res.login);
+    return res;
 }
 function getUser() {
     return localStorage.getItem('github_username');
+}
+
+async function fetchUrl(url) {
+    const res = await fetch(url, {
+        headers: {
+            Authorization: 'token ' + token,
+        },
+    });
+    return await res.json();
 }
 
 function getRepoName() {
@@ -64,8 +57,7 @@ function displayError(context) {
 }
 
 function getNumIssues(milestone) {
-    const { open_issues, closed_issues } = milestone;
-    return open_issues + closed_issues;
+    return milestone.open_issues + milestone.closed_issues;
 }
 
 function setTeamVelocity(milestone) {
@@ -76,55 +68,40 @@ function setTeamVelocity(milestone) {
     //TODO: waiting on team velocity textbox to be added
 }
 
-function setUserVelocity(milestone) {
-    const { number } = milestone;
-    const milestoneId = number;
+async function setUserVelocity(milestone) {
+    const { number: milestoneId } = milestone;
     const userName = getUser();
     const repoName = getRepoName();
-    fetch(
-        `https://api.github.com/repos/${userName}/${repoName}/issues?milestone=${milestoneId}&state=all&assignee=${userName}`,
-        {
-            headers: {
-                Authorization: 'token ' + token,
-            },
-        }
-    )
-        .then(res => res.json())
-        .then(issues => {
-            const closedIssues = issues.filter(
-                issue => issue.state === 'closed'
-            );
-            if (issues.length === 0) {
-                speed.innerHTML = speedErrorMessage;
-                userVelocity = 0;
-            } else {
-                userVelocity = closedIssues.length / issues.length;
-            }
-            /* animation speed will be .2 in the fastest case.
-            this subtraction is done to flip higher values with
-            lower values since a lower number results in a faster
-            animation playback speed. (without this,
-            a velocity of .75 would be slower than .25)
-            to better distribute the times, we use a square
-            function to make it more visible and divide to
-            scale it appropriately. These were chosen through
-            trial and error */
-            const animationSpeed = Math.pow(5 * (1.2 - userVelocity), 2.0) / 10;
-            document.documentElement.style.setProperty(
-                '--raptorSpeed',
-                animationSpeed + 's'
-            );
-            const speedText = Math.floor(userVelocity * 100);
-            if (issues.length !== 0 || milestones.length !== 0) {
-                speed.innerHTML = `Raptor's speed: ${speedText}km/h`;
-            }
-        });
+    const url = `https://api.github.com/repos/${userName}/${repoName}/issues?milestone=${milestoneId}&state=all&assignee=${userName}`;
+    const issues = await fetchUrl(url);
+    const closedIssues = issues.filter(issue => issue.state === 'closed');
+    if (issues.length === 0) displayError(NO_ISSUES);
+
+    let userVelocity =
+        issues.length > 0 ? closedIssues.length / issues.length : 0;
+    /* animation speed will be .2 in the fastest case.
+    this subtraction is done to flip higher values with
+    lower values since a lower number results in a faster
+    animation playback speed. (without this,
+    a velocity of .75 would be slower than .25)
+    to better distribute the times, we use a square
+    function to make it more visible and divide to
+    scale it appropriately. These were chosen through
+    trial and error */
+    const animationSpeed = Math.pow(5 * (1.2 - userVelocity), 2.0) / 10;
+    document.documentElement.style.setProperty(
+        '--raptorSpeed',
+        animationSpeed + 's'
+    );
+    const speedText = Math.floor(userVelocity * 100);
+    if (issues.length !== 0 || milestones.length !== 0) {
+        speed.innerHTML = `Raptor's speed: ${speedText}km/h`;
+    }
 }
 
 async function loadVelocity() {
     await setUser();
     const milestone = await getMostRecentMilestone();
-    console.log(milestone);
     if (!milestone) {
         displayError(NO_MILESTONE);
         return;
@@ -134,113 +111,27 @@ async function loadVelocity() {
         displayError(NO_ISSUES);
         return;
     }
-    await setTeamVelocity(milestone);
+    //TODO: waiting on team velocity textbox
+    //await setTeamVelocity(milestone);
     await setUserVelocity(milestone);
 }
 
-if (localStorage.length != 0) {
-    //get everything and activate listeners to dress raptor with info from other page
-    ///switch cases with same ids for both
-    const list = ['tail', 'back', 'head', 'hand'];
-    const token = localStorage.getItem('token');
-    for (bodyPart of list) {
-        if (localStorage.getItem(bodyPart) != null) {
-            document.getElementById(
-                localStorage.getItem(bodyPart)
-            ).style.display = 'block';
+function initAccessories() {
+    if (localStorage.length != 0) {
+        /* get everything and activate listeners to dress raptor
+           with info from other page switch cases with
+           same ids for both
+        */
+        const list = ['tail', 'back', 'head', 'hand'];
+        const token = localStorage.getItem('token');
+        for (bodyPart of list) {
+            if (localStorage.getItem(bodyPart) != null) {
+                document.getElementById(
+                    localStorage.getItem(bodyPart)
+                ).style.display = 'block';
+            }
         }
     }
-    loadVelocity();
-    // fetch('https://api.github.com/user', {
-    //     headers: {
-    //         Authorization: 'token ' + token,
-    //     },
-    // })
-    //     .then(res => res.json())
-    //     .then(name => {
-    //         localStorage.setItem('github_username', name.login);
-    //         const userName = localStorage.getItem('github_username');
-    //         const repo = JSON.parse(localStorage.getItem('repository'));
-    //         const repoName = repo.repoId;
-    //         const speed = document.getElementById('div-4');
-
-    //         fetch(
-    //             `https://api.github.com/repos/${userName}/${repoName}/milestones`,
-    //             {
-    //                 headers: {
-    //                     //get all issues
-    //                     Authorization: 'token ' + token,
-    //                 },
-    //             }
-    //         )
-    //             .then(res => res.json())
-    //             .then(milestones => {
-    //                 if (milestones.length === 0) {
-    //                     speed.innerHTML = speedErrorMessage;
-    //                     return;
-    //                 }
-    //                 const curMilestone = milestones[0];
-    //                 const { open_issues, closed_issues, number } = curMilestone;
-    //                 const milestoneId = number;
-    //                 const totalIssues = open_issues + closed_issues;
-    //                 let userVelocity;
-    //                 let teamVelocity;
-    //                 if (totalIssues === 0) {
-    //                     speed.innerHTML = speedErrorMessage;
-    //                     teamVelocity = 0;
-    //                 } else {
-    //                     teamVelocity = closed_issues / totalIssues;
-
-    //                     //get user velocity
-    //                     fetch(
-    //                         `https://api.github.com/repos/${userName}/${repoName}/issues?milestone=${milestoneId}&state=all&assignee=${userName}`,
-    //                         {
-    //                             headers: {
-    //                                 Authorization: 'token ' + token,
-    //                             },
-    //                         }
-    //                     )
-    //                         .then(res => res.json())
-    //                         .then(issues => {
-    //                             const closedIssues = issues.filter(
-    //                                 issue => issue.state === 'closed'
-    //                             );
-    //                             if (issues.length === 0) {
-    //                                 speed.innerHTML = speedErrorMessage;
-    //                                 userVelocity = 0;
-    //                             } else {
-    //                                 userVelocity =
-    //                                     closedIssues.length / issues.length;
-    //                                 /* animation speed will be .2 in the fastest case.
-    //                                 this subtraction is done to flip higher values with
-    //                                 lower values since a lower number results in a faster
-    //                                 animation playback speed. (without this,
-    //                                 a velocity of .75 would be slower than .25)
-    //                                 to better distribute the times, we use a square
-    //                                 function to make it more visible and divide to
-    //                                 scale it appropriately. These were chosen through
-    //                                 trial and error */
-    //                                 const animationSpeed =
-    //                                     Math.pow(
-    //                                         5 * (1.2 - userVelocity),
-    //                                         2.0
-    //                                     ) / 10;
-    //                                 document.documentElement.style.setProperty(
-    //                                     '--raptorSpeed',
-    //                                     animationSpeed + 's'
-    //                                 );
-    //                                 const speedText = Math.floor(
-    //                                     userVelocity * 100
-    //                                 );
-    //                                 if (
-    //                                     issues.length !== 0 ||
-    //                                     milestones.length !== 0
-    //                                 ) {
-    //                                     speed.innerHTML = `Raptor's speed: ${speedText}km/h`;
-    //                                 }
-    //                             }
-    //                         });
-    //                 }
-    //             });
-    //     });
 }
+initAccessories();
+loadVelocity();
