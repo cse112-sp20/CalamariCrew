64e79f05357784ff9424296090d9ed8e4da13b1b
KenLHua <32448050+KenLHua@users.noreply.github.com>
2020-06-08 02:55:19 -0700

    Modify remove_all.js to be testable and created the test

--- root/src/js/remove_all.js
+++ root/src/js/remove_all.js
@@ -1,23 +1,31 @@
-const rm = document.getElementById('remove_all');
+var rm = document.getElementById('remove_all');
 var delayInMilliseconds = 1700; //1 second
 
 function restartGif(imgElement) {
     let element = document.getElementById(imgElement);
     element.style.zIndex = 3;
+
     if (element) {
         var imgSrc = element.src;
         element.src = imgSrc;
     }
 }
 
-rm.addEventListener('click', () => {
-    restartGif('meteor');
-    setTimeout(() => {
-        window.localStorage.removeItem('github_username');
-        window.localStorage.removeItem('token');
-        window.localStorage.removeItem('raptor_name');
-        // fetch('http://localhost:3000/clear');
-        document.getElementById('clearAll').click();
-        window.location.href = '/root/html/setup/auth.html';
-    }, delayInMilliseconds);
-});
+if (rm) {
+    rm.addEventListener('click', () => {
+        restartGif('meteor');
+        setTimeout(removeAllData, delayInMilliseconds);
+    });
+}
+
+function removeAllData() {
+    window.localStorage.removeItem('github_username');
+    window.localStorage.removeItem('token');
+    window.localStorage.removeItem('raptor_name');
+    // fetch('http://localhost:3000/clear');
+    document.getElementById('clearAll').click();
+    window.location.href = '/root/html/setup/auth.html';
+}
+
+exports.removeAllData = removeAllData;
+exports.restartGif = restartGif;
