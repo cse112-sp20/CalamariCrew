f024590d7c65ad39617d9856da71195d49aa1180
Azirly <jylin85@gmail.com>
2020-06-11 13:26:03 -0700

    Repo.js Test #1

--- root/src/js/github/repo.js
+++ root/src/js/github/repo.js
@@ -5,49 +5,74 @@ var highlightedRow = {
     issueUrl: undefined,
 };
 
-// Call the user info API using the fetch browser library
-fetch('https://api.github.com/user/repos', {
-    headers: {
-        // Include the token in the Authorization header
-        Authorization: 'token ' + token,
-    },
-})
-    // Parse the response as JSON
-    .then(res => res.json())
-    .then(res => {
-        let repoTableBody = document.getElementById('repoTableBody');
-        res.forEach(repo => {
-            if (repo.name) {
-                let issuesUrl = repo.issues_url.replace('{/number}', '');
-                let tableCell = document.createElement('td');
-                tableCell.innerHTML = repo.name;
-                tableCell.setAttribute('id', repo.name);
-                tableCell.setAttribute('issueUrl', issuesUrl);
-                tableCell.onclick = function() {
-                    if (highlightedRow.repoId) {
-                        document
-                            .getElementById(highlightedRow.repoId)
-                            .removeAttribute('bgcolor');
-                    }
+export function checkRow(tableCell) {
+    if (highlightedRow.repoId) {
+        document
+            .getElementById(highlightedRow.repoId)
+            .removeAttribute('bgcolor');
+    }
 
-                    highlightedRow.repoId = this.id;
-                    highlightedRow.issueUrl = this.getAttribute('issueUrl');
-                    this.setAttribute('bgcolor', '#cebfff');
-                };
+    highlightedRow.repoId = tableCell.id;
+    highlightedRow.issueUrl = tableCell.getAttribute('issueUrl');
+    tableCell.setAttribute('bgcolor', '#cebfff');
 
-                let tableRow = document.createElement('tr');
-                tableRow.appendChild(tableCell);
+    if (tableCell) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-                repoTableBody.appendChild(tableRow);
-            }
+export function tableCellUpdate(tableCell, repo, issuesUrl) {
+    tableCell.innerHTML = repo.name;
+    tableCell.setAttribute('id', repo.name);
+    tableCell.setAttribute('issueUrl', issuesUrl);
+    tableCell.onclick = checkRow(tableCell);
+    return tableCell;
+}
+
+export function tableRowUpdate(tableCell, repoTableBody) {
+    let tableRow = document.createElement('tr');
+    tableRow.appendChild(tableCell);
+
+    repoTableBody.appendChild(tableRow);
+    return repoTableBody;
+}
+
+export async function main() {
+    // Call the user info API using the fetch browser library
+    fetch('https://api.github.com/user/repos', {
+        headers: {
+            // Include the token in the Authorization header
+            Authorization: 'token ' + token,
+        },
+    })
+        // Parse the response as JSON
+        .then(res => res.json())
+        .then(res => {
+            let repoTableBody = document.getElementById('repoTableBody');
+            res.forEach(repo => {
+                if (repo.name) {
+                    let issuesUrl = repo.issues_url.replace('{/number}', '');
+                    let tableCell = document.createElement('td');
+
+                    tableCellUpdate(tableCell, repo, issuesUrl);
+
+                    tableRowUpdate(tableCell, repoTableBody);
+                }
+            });
         });
-    });
+}
 
 var submitButton = document.getElementById('selectRepoButton');
 
-submitButton.addEventListener('click', function() {
-    if (highlightedRow.repoId) {
-        localStorage.setItem('repository', JSON.stringify(highlightedRow));
-        window.location.href = '/root/html/index.html';
-    }
-});
+if (submitButton) {
+    submitButton.addEventListener('click', function() {
+        if (highlightedRow.repoId) {
+            localStorage.setItem('repository', JSON.stringify(highlightedRow));
+            window.location.href = '/root/html/index.html';
+        }
+    });
+}
+
+main();
