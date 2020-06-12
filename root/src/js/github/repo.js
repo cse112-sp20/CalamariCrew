72596eb51233649307f96c33696a11f38e472657
Azirly <jylin85@gmail.com>
2020-06-11 17:25:57 -0700

    Repo Refactoring #15

--- root/src/js/github/repo.js
+++ root/src/js/github/repo.js
@@ -20,46 +20,51 @@ export function tableRowUpdate(tableCell, repoTableBody) {
     return repoTableBody;
 }
 
-// Call the user info API using the fetch browser library
-fetch('https://api.github.com/user/repos', {
-    headers: {
-        // Include the token in the Authorization header
-        Authorization: 'token ' + token,
-    },
-})
-    // Parse the response as JSON
-    .then(res => res.json())
-    .then(res => {
-        let repoTableBody = document.getElementById('repoTableBody');
-        res.forEach(repo => {
-            if (repo.name) {
-                let issuesUrl = repo.issues_url.replace('{/number}', '');
-                let tableCell = document.createElement('td');
+export async function main() {
+    // Call the user info API using the fetch browser library
+    fetch('https://api.github.com/user/repos', {
+        headers: {
+            // Include the token in the Authorization header
+            Authorization: 'token ' + token,
+        },
+    })
+        // Parse the response as JSON
+        .then(res => res.json())
+        .then(res => {
+            let repoTableBody = document.getElementById('repoTableBody');
+            res.forEach(repo => {
+                if (repo.name) {
+                    let issuesUrl = repo.issues_url.replace('{/number}', '');
+                    let tableCell = document.createElement('td');
 
-                tableCellUpdate(tableCell, repo, issuesUrl);
+                    tableCellUpdate(tableCell, repo, issuesUrl);
 
-                tableCell.onclick = function() {
-                    if (highlightedRow.repoId) {
-                        document
-                            .getElementById(highlightedRow.repoId)
-                            .removeAttribute('bgcolor');
-                    }
+                    tableCell.onclick = function() {
+                        if (highlightedRow.repoId) {
+                            document
+                                .getElementById(highlightedRow.repoId)
+                                .removeAttribute('bgcolor');
+                        }
 
-                    highlightedRow.repoId = this.id;
-                    highlightedRow.issueUrl = this.getAttribute('issueUrl');
-                    this.setAttribute('bgcolor', '#cebfff');
-                };
+                        highlightedRow.repoId = this.id;
+                        highlightedRow.issueUrl = this.getAttribute('issueUrl');
+                        this.setAttribute('bgcolor', '#cebfff');
+                    };
 
-                tableRowUpdate(tableCell, repoTableBody);
-            }
+                    tableRowUpdate(tableCell, repoTableBody);
+                }
+            });
         });
-    });
+}
 
 var submitButton = document.getElementById('selectRepoButton');
 
-submitButton.addEventListener('click', function() {
-    if (highlightedRow.repoId) {
-        localStorage.setItem('repository', JSON.stringify(highlightedRow));
-        window.location.href = '/root/html/index.html';
-    }
-});
+if (submitButton) {
+    submitButton.addEventListener('click', function() {
+        if (highlightedRow.repoId) {
+            localStorage.setItem('repository', JSON.stringify(highlightedRow));
+            window.location.href = '/root/html/index.html';
+        }
+    });
+}
+main();
